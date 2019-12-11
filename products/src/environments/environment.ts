2796938e5e633d3bef1202c1cecbbd258e25a9b2
anxo-cloud <anxobreoganFuentes@gmail.com>
2019-12-11 11:47:25 +0100

    firebase instalado

--- products/src/environments/environment.ts
+++ products/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDCQBV9dXizw0Zx63VgfPF0mDns-Siedyg",
+    authDomain: "products-firebase-d8802.firebaseapp.com",
+    databaseURL: "https://products-firebase-d8802.firebaseio.com",
+    projectId: "products-firebase-d8802",
+    storageBucket: "products-firebase-d8802.appspot.com",
+    messagingSenderId: "765256198435",
+    appId: "1:765256198435:web:db135b34c8d899180bc496",
+    measurementId: "G-SXFDMVY5DL"
+  }
 };
 
 /*
