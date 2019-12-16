487c7cf88a899f060d5a075db3c34cfb91b943f6
anxo-cloud <anxobreoganFuentes@gmail.com>
2019-12-16 10:54:54 +0100

    componets creados

--- products/src/app/app.module.ts
+++ products/src/app/app.module.ts
@@ -8,16 +8,27 @@ import { AppComponent } from './app.component';
 
 import {AngularFireModule} from '@angular/fire';
 import { environment } from '../environments/environment';
+//Components
+import { ProductsComponent } from './components/products/products.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { ProductComponent } from './components/products/product/product.component';
+
+//Services
+
+import {ProductService} from './services/product.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
