1adf64e3d5b7468a7bc0e18fd52c14874d30d11f
anxo-cloud <anxobreoganFuentes@gmail.com>
2019-12-16 11:17:10 +0100

    recoger todos los productos

--- products/src/app/services/product.service.ts
+++ products/src/app/services/product.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  productList: AngularFireList<any>;
+  constructor(private firebase: AngularFireDatabase) { }
 
-  constructor() { }
+  //Obtener todos los productos.
+  getProduct(){
+    return this.productList=this.firebase.list('products');
+  }
 }
